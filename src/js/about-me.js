0e884828d853e73d6c62b74b59b4fe79a9a116ba
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2024-09-22 17:18:27 +0100

    add about-me section

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,37 +1,31 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Keyboard, Navigation } from 'swiper/modules';
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
-const acc = new Accordion('.accordion-container', {
-  duration: 500,
-  elementClass: 'ac',
-  triggerClass: 'ac-trigger',
-  panelClass: 'ac-panel',
+new Accordion('.accordion-container', {
   showMultiple: true,
-  openOnInit: [],
+  openOnInit: [0],
 });
 
-acc.open(0);
-
-// SWIPER INICIALISATION
-
-new Swiper('.swiper-of-skills', {
-  speed: 400,
+new Swiper('#about-me .swiper', {
+  modules: [Keyboard, Navigation, Mousewheel],
   slidesPerView: 2,
   spaceBetween: 0,
-  modules: [Keyboard, Navigation],
-  edgeSwipeDetection: true,
-  grabCursor: true,
   loop: true,
-  noSwipingSelector: '.skills-swiper-button-next',
-  slideActiveClass: 'accent-color',
+  allowSlidePrev: false,
+  speed: 600,
+  edgeSwipeDetection: true,
+  updateOnWindowResize: true,
+  keyboard: true,
+  mousewheel: true,
+  slideActiveClass: 'skills-active-slide',
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+  on: {
+    keyPress: onMyKeyPress,
+  },
   breakpoints: {
-    320: {
-      slidesPerView: 2,
-    },
     768: {
       slidesPerView: 3,
     },
@@ -39,26 +33,12 @@ new Swiper('.swiper-of-skills', {
       slidesPerView: 6,
     },
   },
-  on: {
-    keyPress: onMyKeyPress,
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  navigation: {
-    nextEl: '.skills-swiper-button-next',
-  },
 });
 
 function onMyKeyPress(swiper, keyCode) {
   switch (keyCode) {
     case 9: //Tab
       swiper.slideNext();
-      break;
-    case 8: //Backspace
-      swiper.slidePrev();
       break;
   }
 }
--- working-materials.md
+++ working-materials.md
@@ -22,7 +22,23 @@ box-shadow: 0px 0px 12px 4px #ff7f08;
 transform: scale(1.15); stroke: #ff7f08; filter: drop-shadow(0px 0px 5px
 #ff7f08);
 
-<!-- *Btns hover -->
+<!-- *Color Btns hover -->
 
 background-color: rgba(255, 127, 8, 0.3); color: #fafafa; box-shadow: inset 0px
 0px 12px 4px #ff7f08;
+
+<!-- *Hero socials -->
+
+background-color: rgba(28, 29, 32, 0.3); box-shadow: 0px 0px 4px 0px #ff7f08;
+
+<!--? ========================================== -->
+
+<!-- *Accordion hover (BUBBLE) -->
+
+transform: scale(1.15); stroke: #ff7f08; filter: drop-shadow(0px 0px 5px
+#ff7f08); border: 1px solid rgba(255, 127, 8, 0.2); box-shadow: 0px 0px 4px 0px
+#ff7f08;
+
+<!-- *Accordion title text -->
+
+color: rgba(250, 250, 250, 0.8); filter: drop-shadow(0px 0px 5px #ff7f08);
